3a18b10fe95e85749587e2269a065596db787649
Juan Martín Carsillo <juanmartincarsillo@gmail.com>
2022-06-22 19:18:27 -0300

    dasafio clase 4

--- script.js
+++ script.js
@@ -17,35 +17,35 @@ const ROLL9= new Rolls(4,"Langos, Ome, Palta, Queso", 789);
 
 
 while(true){
-    let opcion = parseInt(prompt("Ingresá el número del roll del cuál quieras averiguar el precio. Para finalizar ingresar el número 10."));
+    let opcion = parseInt(prompt("Ingresá un número del 1 al 9 para conocer nuestros rolls y sus precios. Para finalizar ingresá el número 10."));
     let costo = 0
     if(opcion == 1){
         costo = ROLL1.precio;
-        alert("El roll cuesta: $ " + costo);
+        alert("Las " + ROLL1.cantidad + " piezas de " + ROLL1.ingredientes + " cuestan: $ " + costo / 2 );
     }else if(opcion == 2){
         costo = ROLL2.precio;
-        alert("El roll cuesta: $ " + costo);
+        alert("Las " + ROLL2.cantidad + " piezas de " + ROLL2.ingredientes + " cuestan: $ " + costo / 2)
     }else if(opcion == 3){
         costo = ROLL3.precio;
-        alert("El roll cuesta: $ " + costo);
+        alert("Las " + ROLL3.cantidad + " piezas de " + ROLL3.ingredientes + " cuestan: $ " + costo / 2)
     }else if(opcion == 4){
         costo = ROLL4.precio;
-        alert("El roll cuesta: $ " + costo);
+        alert("Las " + ROLL4.cantidad + " piezas de " + ROLL4.ingredientes + " cuestan: $ " + costo / 2)
     }else if(opcion == 5){
         costo = ROLL5.precio;
-        alert("El roll cuesta: $ " + costo);
+        alert("Las " + ROLL5.cantidad + " piezas de " + ROLL5.ingredientes + " cuestan: $ " + costo / 2)
     }else if(opcion == 6){
         costo = ROLL6.precio;
-        alert("El roll cuesta: $ " + costo);
+        alert("Las " + ROLL6.cantidad + " piezas de " + ROLL6.ingredientes + " cuestan: $ " + costo / 2)
     }else if(opcion == 7){
         costo = ROLL7.precio;
-        alert("El roll cuesta: $ " + costo);
+        alert("Las " + ROLL7.cantidad + " piezas de " + ROLL7.ingredientes + " cuestan: $ " + costo / 2)
     }else if(opcion == 8){
         costo = ROLL8.precio;
-        alert("El roll cuesta: $ " + costo);
+        alert("Las " + ROLL8.cantidad + " piezas de " + ROLL8.ingredientes + " cuestan: $ " + costo / 2)
     }else if(opcion == 9){
         costo = ROLL9.precio;
-        alert("El roll cuesta: $ " + costo);
+        alert("Las " + ROLL9.cantidad + " piezas de " + ROLL9.ingredientes + " cuestan: $ " + costo / 2)
     }else if(opcion == 10){
         break;
     }else{
